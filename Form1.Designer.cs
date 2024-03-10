e17a166357732b12e9c4d305d09b1974f1b780c0
Andrei <oranceanuandrei@gmail.com>
2024-03-10 23:56:42 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guard1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.guard2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.time = new System.Windows.Forms.Label();
+            this.time3 = new System.Windows.Forms.Label();
+            this.time2 = new System.Windows.Forms.Label();
+            this.sc2 = new System.Windows.Forms.Label();
+            this.sc1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.guard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guard1
+            // 
+            this.guard1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.guard1.Location = new System.Drawing.Point(766, 184);
+            this.guard1.Name = "guard1";
+            this.guard1.Size = new System.Drawing.Size(21, 79);
+            this.guard1.TabIndex = 0;
+            this.guard1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ball.Location = new System.Drawing.Point(290, 150);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // guard2
+            // 
+            this.guard2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.guard2.Location = new System.Drawing.Point(12, 184);
+            this.guard2.Name = "guard2";
+            this.guard2.Size = new System.Drawing.Size(20, 78);
+            this.guard2.TabIndex = 2;
+            this.guard2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Location = new System.Drawing.Point(396, -16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 564);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.time.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.time.Location = new System.Drawing.Point(629, 21);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(48, 36);
+            this.time.TabIndex = 4;
+            this.time.Text = "1 :";
+            this.time.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // time3
+            // 
+            this.time3.AutoSize = true;
+            this.time3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.time3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.time3.Location = new System.Drawing.Point(712, 21);
+            this.time3.Name = "time3";
+            this.time3.Size = new System.Drawing.Size(32, 36);
+            this.time3.TabIndex = 5;
+            this.time3.Text = "0";
+            // 
+            // time2
+            // 
+            this.time2.AutoSize = true;
+            this.time2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.time2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.time2.Location = new System.Drawing.Point(683, 21);
+            this.time2.Name = "time2";
+            this.time2.Size = new System.Drawing.Size(32, 36);
+            this.time2.TabIndex = 6;
+            this.time2.Text = "0";
+            // 
+            // sc2
+            // 
+            this.sc2.AutoSize = true;
+            this.sc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sc2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sc2.Location = new System.Drawing.Point(323, 397);
+            this.sc2.Name = "sc2";
+            this.sc2.Size = new System.Drawing.Size(42, 46);
+            this.sc2.TabIndex = 7;
+            this.sc2.Text = "0";
+            // 
+            // sc1
+            // 
+            this.sc1.AutoSize = true;
+            this.sc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sc1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sc1.Location = new System.Drawing.Point(428, 397);
+            this.sc1.Name = "sc1";
+            this.sc1.Size = new System.Drawing.Size(42, 46);
+            this.sc1.TabIndex = 8;
+            this.sc1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sc1);
+            this.Controls.Add(this.sc2);
+            this.Controls.Add(this.time2);
+            this.Controls.Add(this.time3);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.guard2);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.guard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.guard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox guard1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox guard2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label time3;
+        private System.Windows.Forms.Label time2;
+        private System.Windows.Forms.Label sc2;
+        private System.Windows.Forms.Label sc1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Up && guard1.Top > 0)
+            {
+                guard1.Top -= 10;
+            }
+            else if (keyData == Keys.Down && guard1.Top < 300)
+            {
+                guard1.Top += 10;
+            }
+            
+            return base.ProcessCmdKey(ref msg, keyData);
+        }*/
+
+        private int x = 1, y = 0, z = 0, score1 = 0, score2 = 0;
+        private bool right = true, left = false, up = false, down = true,down1 = false,up1 = false,down2 = false, up2 = false;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time.Text = x.ToString();
+            time2.Text = y.ToString();
+            time3.Text = z.ToString();
+            if(z == 0)
+            {
+                if(x == 0 && y == 0)
+                {
+                    timer2.Enabled = false;
+                    timer1.Enabled = false;
+                    if (score2 > score1)
+                    {
+                        sc2.ForeColor = Color.Green;
+                        sc1.ForeColor = Color.Red;
+                    }else if (score1 > score2)
+                    {
+                        sc1.ForeColor = Color.Green;
+                        sc2.ForeColor = Color.Red;
+                    }else if(score1 == score2){
+                        sc1.ForeColor = Color.Green;
+                        sc2.ForeColor = Color.Green;
+                    }
+                    time.Text = "0";
+                    time2.Text = "0";
+                    time3.Text = "0";
+                    
+                }
+                z = 9;
+                if(y == 0)
+                {
+                    y = 5;
+                    x--;
+                }
+                else
+                {
+                    y--;
+                }
+            }
+            else
+            {
+                z--;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                up1 = false;
+            }else if(e.KeyCode == Keys.Down)
+            {
+                down1 = false;
+            }else if(e.KeyCode == Keys.W)
+            {
+                up2 = false;
+            }else if(e.KeyCode == Keys.S)
+            {
+                down2 = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                up1 = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                down1 = true;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                up2 = true;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                down2 = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(up1 == true)
+            {
+                guard1.Top-=2;
+            }
+            if(up2 == true)
+            {
+                guard2.Top -= 2;
+            }
+            if(down1 == true)
+            {
+                guard1.Top += 2;
+            }
+            if(down2 == true)
+            {
+                guard2.Top += 2;
+            }
+
+            
+            if(ball.Top < 5)
+            {
+                up = false;
+                down = true;
+            }
+            if(ball.Top > 350)
+            {
+                up = true;
+                down = false;
+            }
+            
+            if((ball.Left + 20 == guard1.Left && ball.Top +20 > guard1.Top&&ball.Top<guard1.Top + 78)||(ball.Left < guard2.Left + 20 && ball.Top + 20 > guard2.Top && ball.Top < guard2.Top + 78))
+            {
+                if(right == true)
+                {
+                    right = false;
+                    left = true;
+                }else if(left == true)
+                {
+                    left = false;
+                    right = true;
+                }
+            }
+            if (ball.Left + 20 > 620)
+            {
+                score2++;
+                right = true;
+                down = true;
+                left = false;
+                up = false;
+                ball.Left = 290;
+                ball.Top = 150;
+            }
+            if(ball.Left < -20)
+            {
+                score1++;
+                right = true;
+                down = true;
+                left = false;
+                up = false;
+                ball.Left = 290;
+                ball.Top = 150;
+            }
+
+            if(left == true && down == true)
+            {
+                ball.Top += 3;
+                ball.Left -= 3;
+            }
+            if(left == true && up == true)
+            {
+                ball.Top -= 3;
+                ball.Left -= 3;
+            }
+            if(right == true&&down == true)
+            {
+                ball.Top += 3;
+                ball.Left += 3;
+            }
+            if(right == true && up == true)
+            {
+                ball.Top -= 3;
+                ball.Left += 3;
+            }
+            sc1.Text = score1.ToString();
+            sc2.Text = score2.ToString();
+        }
+    }
+}
